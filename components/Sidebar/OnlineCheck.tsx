074e0b88a208b6656054fb2dd0b42b397e6503e2
Aditya Giri <dtrg21@gmail.com>
2023-01-08 22:12:29 +0530

    [fix] Add proper online loader status

--- components/Sidebar/OnlineCheck.tsx
+++ components/Sidebar/OnlineCheck.tsx
@@ -14,11 +14,11 @@ export default function OnlineCheck() {
 
   if (isLoading) {
     return (
-      <span className="mt-3 inline-flex items-center rounded-full bg-red-100 px-3 py-0.5 text-sm font-medium text-red-800">
-        <svg className="-ml-1 mr-1.5 h-2 w-2 text-red-400" fill="currentColor" viewBox="0 0 8 8">
+      <span className="mt-3 inline-flex items-center rounded-full bg-green-100 px-3 py-0.5 text-sm font-medium text-green-800">
+        <svg className="animate-ping -ml-1 mr-1.5 h-2 w-2 text-green-400" fill="currentColor" viewBox="0 0 8 8">
           <circle cx={4} cy={4} r={3} />
         </svg>
-        Offline
+        Loading Status...
       </span>
     );
   }
